d9298925a7d8dee176e04053a8c7e3baa3a6628f
Diego Cardozo <diegocardozo2712@outlook.com>
2023-04-17 18:43:56 -0300

    Update tarea1.pas

--- tarea1.pas
+++ tarea1.pas
@@ -30,7 +30,6 @@ begin
         if (esVocal(oracion)) and (oracion <> 'u') and (vocal1 <> 'q') then
             write ('p',oracion)
         
-        else if 
 
         else if (lacu = 'q') and (vocal1 = 'u') and (oracion = 'e') or (oracion = 'i') then
             write('p',oracion);
