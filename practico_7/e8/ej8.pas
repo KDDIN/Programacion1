abbcbbc6fbae48a99f4e698cf7bed2c2911d656a
Diego Cardozo <diegocardozo2712@outlook.com>
2023-05-03 15:10:27 -0300

    p7 e8

---
+++ practico_7/e8/ej8.pas
@@ -0,0 +1,39 @@
+program practico7_ejercicio8;
+
+var n:integer;
+
+{Inicio de la funcion}
+function esPrimo (primo:integer) : boolean;
+
+var divisor: Integer;
+
+begin
+
+    divisor := 2;
+
+    if (primo mod 2 = 0) and (primo > 2) then
+    begin
+
+        repeat
+            primo := primo +1;
+        until (primo mod divisor = 0) or (primo = divisor);
+
+        esPrimo := (not (primo mod divisor = 0) or (primo = divisor));
+
+    end
+    else
+        esPrimo := false;
+
+end;
+
+{Inicio del programa}
+begin
+
+    Write('Ingrese un numero: ');
+    Readln(n);
+    if esPrimo(n) then
+        writeln(n,' Es primo');
+    else
+        writeln(n,' No es primo');
+
+end.
