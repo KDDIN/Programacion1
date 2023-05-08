f414e92f1eaf17d99d651fd509fc232aa9be1562
Diego Cardozo <diegocardozo2712@outlook.com>
2023-05-08 16:52:22 -0300

    crear y ordenar carpetas y archivos

---
+++ practico_9/e2/b/ej2b.pas
@@ -0,0 +1,14 @@
+Program Practico9_ejercicio2b;
+
+const N = 5;
+
+type CadenaN = array [1..N] of integer;
+
+function ordenado(cadn:CadenaN) : boolean;
+
+var i: integer;
+
+begin
+
+
+end;
---
+++ practico_9/e2/c/ej2c.pas
@@ -0,0 +1,7 @@
+Program Practico9_ejercicio2c;
+
+const N = 5;
+
+type CadenaN = array [1..N] of integer;
+
+procedure maxValorPos(cadn:CadenaN; VAR valor, pos:integer)
