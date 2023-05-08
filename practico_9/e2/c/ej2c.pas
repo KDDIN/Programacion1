c4736fdd63bfb0c152d57675afa4c37d39450c46
Diego Cardozo <diegocardozo2712@outlook.com>
2023-05-08 17:46:13 -0300

    Update ej2c.pas

--- practico_9/e2/c/ej2c.pas
+++ practico_9/e2/c/ej2c.pas
@@ -4,4 +4,44 @@ const N = 5;
 
 type CadenaN = array [1..N] of integer;
 
-procedure maxValorPos(cadn:CadenaN; VAR valor, pos:integer)
+var
+    r,v,p: integer;
+    fila: CadenaN;
+
+{indice := N;
+for i := N-1 downto 1 do
+    if arreglo[i] <= arreglo[indice] then
+        indice := i
+}
+{Inicio del procedimiento}
+procedure maxValorPos(cadn:CadenaN; VAR valor, pos:integer);
+
+var i : integer;
+
+begin
+
+    valor := cadn[N];
+    pos := N;
+
+    for i := N-1 downto 1 do
+        if cadn[i] >= cadn[pos] then
+        begin
+            pos := i;
+            valor := cadn[i];
+        end;
+
+end;
+
+{Inicio del programa}
+begin
+
+    for r := N downto 1 do {llenamos el array con numeros del 1 al 5}
+    begin
+        fila[r] := r;
+    end;
+
+    maxValorPos(fila,v,p);
+
+    Writeln('El mayor valor del array esta en la posicion ',p,' y su valor es ',v);
+
+end.
