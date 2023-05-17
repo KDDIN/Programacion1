bb46e2ad2a55d2665e966163bc4a3f6c0d9d4f3c
Diego Cardozo <diegocardozo2712@outlook.com>
2023-05-17 14:46:33 -0300

    Update ej6.pas

--- practico_10/e6/ej6.pas
+++ practico_10/e6/ej6.pas
@@ -2,9 +2,12 @@ program P10EJ6;
 
 type 
     TipoMes = (enero,febrero,marzo,abril,mayo,junio,julio,agosto,septiembre,octubre,noviembre,diciembre);
+    TipoMesRango = 1..12;
 
-var unmes:TipoMes;
+var 
+    unmes:TipoMes;
 
+{Inicio del metodo}
 procedure MostrarMes (var mes:TipoMes);
 begin
 
@@ -27,9 +30,23 @@ begin
 
 end;
 
+{Inicio del metodo}
+procedure MostrarMesPorNumero (var mes:TipoMes);
+var numeromes : TipoMesRango;
+begin
+
+    write ('Ingrese un mes del 1 al 12: ');
+    read (numeromes);
+    Writeln('El mes ingresado fue: ', mes(numeromes-1));
+
+end;
+
+
+{Inicio del programa}
 begin
 
     MostrarMes (unmes);
     Writeln;
+    MostrarMesPorNumero(TipoMesRango);
 
 end.
